781fe74d1cd22c07b5c08fde849dbd133d072ee6
root <root@debian.itashi.io>
2020-11-23 17:27:50 -0500

    build

---
+++ patch.sh
@@ -0,0 +1,5 @@
+
+echo "export RSTUDIO_WHICH_R=/app/r/bin/R" >> ~/.bashrc
+echo "source /app/source/intel/compilers_and_libraries/linux/mkl/bin/mklvars.sh intel64" >> ~/.bashrc
+echo "declare -x JAVA_HOME=/app/java/jdk-15.0.1/" >> ~/.bashrc
+
